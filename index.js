e8c74e62b08f024439b38dc823b419670afd8090
Zandan-Ochir Ganbold <zagig35@gmail.com>
2023-03-06 12:50:42 +0800

    get data fixed

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ import calc from "./ap/index.js";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
 // allow CORS
 app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	console.log("requsted accepted");
-	next();
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  console.log("requsted accepted");
+  next();
 });
 
 app.use(cors());
@@ -22,38 +25,40 @@ app.use(cors());
 app.use(bodyParser.json());
 
 app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
+  bodyParser.urlencoded({
+    extended: true,
+  })
 );
 
 app.use("/data", sisiroute);
 app.use("/calc", calc);
 app.get("/sisi", (req, res) => {
-	// console log request header
-	const unitId = req.query.unitId;
-	if (unitId == null) {
-		res.send("unitId is null");
-	}
+  // console log request header
+  const unitId = req.query.unitId;
+  if (unitId == null) {
+    res.send("unitId is null");
+  }
 
-	axios.post(`https://stud-api.num.edu.mn/topMenus/units?unitid=${unitId}`).then((response) => {
-		res.send(response.data);
-	});
-	// res.send("Hello World!");
+  axios
+    .post(`https://stud-api.num.edu.mn/topMenus/units?unitid=${unitId}`)
+    .then((response) => {
+      res.send(response.data);
+    });
+  // res.send("Hello World!");
 });
 
 app.use(express.json({ limit: "1mb" }));
 app.use(express.static("public"));
 
 app.use((req, res, next) => {
-	res.status(404).send("Sorry can't find that!");
+  res.status(404).send("Sorry can't find that!");
 });
 
 app.use((err, req, res, next) => {
-	console.error(err.stack);
-	res.status(500).send("Something broke!");
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 app.listen(port, "0.0.0.0", () => {
-	console.log(`Example app listening at http://0.0.0.0:${port}`);
+  console.log(`Example app listening at http://0.0.0.0:${port}`);
 });
